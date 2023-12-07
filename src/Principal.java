9059c95504827e15db59b9b8e9415f3861e0de5c
joao <joao@linux>
2023-12-07 16:28:08 -0300

    Comparando objetos da forma errada

--- src/Principal.java
+++ src/Principal.java
@@ -4,13 +4,17 @@ import javax.swing.JOptionPane;
 public class Principal {
 
     public static void main(String[] args) {
-        
+
         Motorista m = new Motorista();
         m.setCpf("100200300-40");
         m.setNome("Pedro");
         m.setTelefone("86 99999-8888");
-        
-        JOptionPane.showMessageDialog(null, "Nome: " + m);
 
+        Motorista m2 = new Motorista();
+        m2.setCpf("100200300-40");
+        m2.setNome("Pedro");
+        m2.setTelefone("86 99999-8888");
+        
+        System.out.println(m.equals(m2));
     }
 }
